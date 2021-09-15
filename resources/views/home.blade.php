7934b8c626cc97d48123c24786e8fd3dc98e5279
rafikanh <rafikanurhayati01@gmail.com>
2021-09-15 18:21:03 +0700

    minggu3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+    <title> Home @yield('home')</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="css/templatemo-style.css">                                   <!-- Templatemo style -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <![endif]-->
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="tm-header">
+            <div class="container-fluid">
+                <div class="tm-header-inner">
+                    <div class="navbar-brand tm-site-name">SELAMAT DATANG!</div>
+                    
+                    <!-- navbar -->
+                    <nav class="navbar tm-main-nav">
+                        <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#tmNavbar">
+                            &#9776;
+                        </button>
+                        
+                        <div class="collapse navbar-toggleable-sm" id="tmNavbar">
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item">
+                                    <a href="http://127.0.0.1:8000/profile" class="nav-link">Profile</a>
+                                </li>
+                            </ul>                        
+                        </div>
+                    </nav>  
+                </div>                                  
+            </div>            
+        </div>
+
+        <div class="tm-blog-img-container"></div>
+
+        <section class="tm-section">
+            <div class="container-fluid">
+                <div class="row tm-2-rows-sm-swap">
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 tm-2-rows-sm-down-2">
+                        <h2 class="tm-gold-text">Sidebar</h2>
+                        <nav>
+                            <ul class="nav">
+                                <h3>HALO^^</h3>
+                                <li><a href="https://www.cnbcindonesia.com/tech/20210818104648-37-269242/astronaut-ini-sedih-lihat-bumi-dari-antariksa-ada-apa" class="tm-footer-link">Lihat Bumi Dari Antariksa</a></li>
+                                <li><a href="https://www.tribunnews.com/travel/2020/04/21/kembali-dari-ruang-angkasa-astronot-nasa-terkejut-lihat-kondisi-bumi-yang-berbeda-akibat-covid-19" class="tm-footer-link">Kondisi Bumi Saat Covid-19</a></li>
+                            </ul>
+                        </nav>   
+
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9 col-xl-9 tm-2-rows-sm-down-1">
+                        <h2 class="tm-gold-text">Selamat Datang Teman-Teman!</h2>
+                        <p>Perkenalkan ini website terbaruku^^</p>
+                    </div>
+                    
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="tm-footer-content-box">
+                            <h3 class="tm-gold-text tm-title tm-footer-content-box-title">Astronaut Ini Sedih Lihat Bumi Dari Antariksa, Ada Apa?</h3>
+                            <p class="tm-margin-b-30">Astronaut NASA Megan McArthur, yang berada di stasiun luar angkasa International Space Station (ISS) menyuarakan kesedihannya melihat kondisi Bumi dari luar angkasa. Ada Apa?</p><hr class="tm-margin-b-30">
+                            <p class="tm-margin-b-30">Megan McArthur mengutarakan kesedihannya soal kasus kebakaran hebat yang terjadi di banyak tempat. Bahkan beberapa kebakaran hutan yang besar bisa terlihat dengan mudah dari luar angkasa.</p><hr class="tm-margin-b-30">
+                            <a href="https://www.cnbcindonesia.com/tech/20210818104648-37-269242/astronaut-ini-sedih-lihat-bumi-dari-antariksa-ada-apa" class="tm-btn tm-btn-gray text-uppercase">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="tm-footer-content-box">
+                            <h3 class="tm-gold-text tm-title tm-footer-content-box-title">Kembali dari Ruang Angkasa, Astronot NASA Terkejut Lihat Kondisi Bumi yang Berbeda Akibat Covid-19</h3>
+                            <p class="tm-margin-b-30">Setelah berbulan-bulan berada di ruang angkasa, tiga atronot The National Aeronautics and Space Administration (NASA) akhirnya kembali ke Bumi pada Jumat (17/4/2020).</p><hr class="tm-margin-b-30">
+                            <p class="tm-margin-b-30">Namun Bumi yang dilihat ketiga astronot itu sedikit berbeda dari saat mereka meninggalkannya karena pandemi virus corona (Covid-19).</p><hr class="tm-margin-b-30">
+                            <a href="https://www.tribunnews.com/travel/2020/04/21/kembali-dari-ruang-angkasa-astronot-nasa-terkejut-lihat-kondisi-bumi-yang-berbeda-akibat-covid-19" class="tm-btn tm-btn-gray text-uppercase">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="tm-footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="tm-footer-content-box">
+                                <div class="col-xs-12 tm-copyright-col">
+                                    <p class="tm-copyright-text">Copyright 2021 Rafika Nurhayati</p>
+                                </div>
+                        </div>                        
+                    </div>
+                </div>    
+            </div>
+        </footer>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    <title> Profile @yield('profile')</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="css/templatemo-style.css">                                   <!-- Templatemo style -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <![endif]-->
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="tm-header">
+            <div class="container-fluid">
+                <div class="tm-header-inner">
+                    <div class="navbar-brand tm-site-name">SELAMAT DATANG DI PROFIL SAYA!</div>
+                    
+                    <!-- navbar -->
+                    <nav class="navbar tm-main-nav">
+                        <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#tmNavbar">
+                            &#9776;
+                        </button>
+                        
+                        <div class="collapse navbar-toggleable-sm" id="tmNavbar">
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item">
+                                    <a href="http://127.0.0.1:8000/home" class="nav-link">Home</a>
+                                </li>
+                            </ul>                        
+                        </div>
+                    </nav>  
+                </div>                                  
+            </div>            
+        </div>
+
+        <div class="tm-contact-img-container"></div>
+
+        <section class="tm-section">
+            <div class="container-fluid">
+                <div class="row tm-2-rows-sm-swap">
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 tm-2-rows-sm-down-2">
+                        <h2 class="tm-gold-text">Sidebar</h2>
+                        <nav>
+                            <ul class="nav">
+                                <h3>HALO^^</h3>
+                                <li><a href="https://www.cnbcindonesia.com/tech/20210818104648-37-269242/astronaut-ini-sedih-lihat-bumi-dari-antariksa-ada-apa" class="tm-footer-link">Lihat Bumi Dari Antariksa</a></li>
+                                <li><a href="https://www.tribunnews.com/travel/2020/04/21/kembali-dari-ruang-angkasa-astronot-nasa-terkejut-lihat-kondisi-bumi-yang-berbeda-akibat-covid-19" class="tm-footer-link">Kondisi Bumi Saat Covid-19</a></li>
+                            </ul>
+                        </nav>   
+
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9 col-xl-9 tm-2-rows-sm-down-1">
+                        <h2 class="tm-gold-text">Profil Saya</h2>
+                        <p>Perkenalkan nama saya Rafika Nurhayati</p>
+                        <p>Saya kuliah di Politeknik Negeri Malang</p>
+                        <p>Jurusan D3 Manajemen Informatika</p>
+                        <p>Saya tinggal di Perumahan Permata Candiloka Blok S1/09,</p>
+                        <p>Desa Balonggabus, Kecamatan Candi, Kabupaten Sidoarjo</p>
+                        <p>Hobi saya membaca novel dan menonton film</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="tm-footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                        <div class="tm-footer-content-box">
+                                <div class="col-xs-12 tm-copyright-col">
+                                    <p class="tm-copyright-text">Copyright 2021 Rafika Nurhayati</p>
+                                </div>
+                        </div>                        
+                    </div>
+                </div>    
+            </div>
+        </footer>
+</body>
+</html>
